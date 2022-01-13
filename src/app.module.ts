97703b8bbe49a8e4bf9f94aec5bd0e2fdf58980e
Andrey Araujo <jaab.andrey@gmail.com>
2022-01-12 21:00:03 -0300

    chore: config inicial

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './modules/users/users.module';
-
 @Module({
   imports: [
     ConfigModule.forRoot(),
@@ -19,9 +15,8 @@ import { UsersModule } from './modules/users/users.module';
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true,
     } as TypeOrmModuleOptions),
-    UsersModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
