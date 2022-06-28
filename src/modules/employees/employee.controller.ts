f06d87310a1dce298ba989c7f9bf9a11d39f41db
Andrey Araujo <jaab.andrey@gmail.com>
2022-06-27 21:27:14 -0300

    feat: adiciona listagem de funcionarios com paginacao

--- src/modules/employees/employee.controller.ts
+++ src/modules/employees/employee.controller.ts
@@ -1,19 +1,36 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Pagination } from 'nestjs-typeorm-paginate';
+
+import {
+  Body,
+  Controller,
+  DefaultValuePipe,
+  Get,
+  ParseIntPipe,
+  Post,
+  Query,
+} from '@nestjs/common';
 import {
   ApiBadRequestResponse,
   ApiCreatedResponse,
   ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiQuery,
   ApiTags,
 } from '@nestjs/swagger';
 
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { CreatedEmployeeDto } from './dto/created-employee.dto';
+import { EmployeesEntity } from './entities/employees.entity';
 import { CreateEmployeeService } from './services/application/create-employee/create-employee.service';
+import { ListAllEmployeesService } from './services/application/list-all-employees/list-all-employees.service';
 
 @ApiTags('Employees')
 @Controller('employees')
 export class EmployeesController {
-  constructor(private readonly createEmployeeService: CreateEmployeeService) {}
+  constructor(
+    private readonly createEmployeeService: CreateEmployeeService,
+    private readonly listAllEmployeesService: ListAllEmployeesService,
+  ) {}
 
   @Post()
   @ApiCreatedResponse({
@@ -28,5 +45,49 @@ export class EmployeesController {
   })
   async create(@Body() body: CreateEmployeeDto): Promise<CreatedEmployeeDto> {
     return this.createEmployeeService.execute(body);
+  }
+
+  @Get()
+  @ApiOkResponse({
+    description: 'Listagem feita com sucesso',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Erro interno no servidor',
+  })
+  @ApiBadRequestResponse({
+    description: 'Não foi possível realizar a listagem',
+  })
+  @ApiQuery({
+    description: 'Id da instituição do funcionário',
+    required: false,
+    name: 'institutionId',
+    type: 'string',
+  })
+  @ApiQuery({
+    description: 'Pagina',
+    required: false,
+    name: 'page',
+    type: 'number',
+  })
+  @ApiQuery({
+    description: 'Limite de registros listados',
+    required: false,
+    name: 'limit',
+    type: 'number',
+  })
+  async findAll(
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page = 1,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit = 10,
+    @Query('institutionId') institutionId: string,
+  ): Promise<Pagination<EmployeesEntity>> {
+    limit = limit > 100 ? 100 : limit;
+    return this.listAllEmployeesService.execute(
+      {
+        page,
+        limit,
+        route: '/employees',
+      },
+      institutionId,
+    );
   }
 }
--- src/modules/employees/employees.module.ts
+++ src/modules/employees/employees.module.ts
@@ -4,11 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { EmployeesController } from './employee.controller';
 import { EmployeesEntity } from './entities/employees.entity';
 import { CreateEmployeeService } from './services/application/create-employee/create-employee.service';
+import { ListAllEmployeesService } from './services/application/list-all-employees/list-all-employees.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([EmployeesEntity])],
   controllers: [EmployeesController],
-  providers: [CreateEmployeeService],
+  providers: [CreateEmployeeService, ListAllEmployeesService],
   exports: [],
 })
 export class EmployeesModule {}
