f4acec8f22db38911673fc8d1561b3166eed4c0b
Andrey Araujo <jaab.andrey@gmail.com>
2022-07-06 15:36:54 -0300

    feat: adiciona service para atualizar agenda

--- src/modules/schedule/schedule.controller.ts
+++ src/modules/schedule/schedule.controller.ts
@@ -1,19 +1,35 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import {
   ApiBadRequestResponse,
+  ApiBody,
   ApiCreatedResponse,
   ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiParam,
   ApiTags,
 } from '@nestjs/swagger';
 
 import { CreateScheduleDto } from './dto/create-schedule.dto';
 import { CreatedScheduleDto } from './dto/created-schedule.dto';
+import { UpdateScheduleDto } from './dto/update-schedule.dto';
+import { Schedules } from './entities/schedule.entity';
 import { CreateScheduleService } from './services/application/create-schedule/create-schedule.service';
+import { UpdateScheduleService } from './services/application/update-schedule/update-schedule.service';
 
 @ApiTags('Schedules')
 @Controller('schedules')
 export class ScheduleController {
-  constructor(private readonly createScheduleService: CreateScheduleService) {}
+  constructor(
+    private readonly createScheduleService: CreateScheduleService,
+    private readonly updateScheduleService: UpdateScheduleService,
+  ) {}
 
   @Post()
   @ApiCreatedResponse({
@@ -26,7 +42,35 @@ export class ScheduleController {
   @ApiBadRequestResponse({
     description: 'Não foi possível realizar a criação',
   })
+  @ApiBody({
+    description: 'Body para fazer a requisição',
+    type: CreateScheduleDto,
+  })
   async create(@Body() body: CreateScheduleDto): Promise<CreatedScheduleDto> {
     return this.createScheduleService.execute(body);
+  }
+
+  @Patch(':id')
+  @ApiOkResponse({ description: 'Atualização feita com sucesso' })
+  @ApiParam({
+    name: 'id',
+    description: 'Id da agenda',
+    required: true,
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Erro interno no servidor',
+  })
+  @ApiBadRequestResponse({
+    description: 'Não foi possível realizar a atualização',
+  })
+  @ApiBody({
+    description: 'Body para fazer a requisição',
+    type: UpdateScheduleDto,
+  })
+  async update(
+    @Param('id', new ParseUUIDPipe()) id: string,
+    @Body() body: UpdateScheduleDto,
+  ): Promise<Schedules> {
+    return this.updateScheduleService.execute(body, id);
   }
 }
--- src/modules/schedule/schedule.module.ts
+++ src/modules/schedule/schedule.module.ts
@@ -4,10 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Schedules } from './entities/schedule.entity';
 import { ScheduleController } from './schedule.controller';
 import { CreateScheduleService } from './services/application/create-schedule/create-schedule.service';
+import { UpdateScheduleService } from './services/application/update-schedule/update-schedule.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Schedules])],
-  providers: [CreateScheduleService],
+  providers: [CreateScheduleService, UpdateScheduleService],
   controllers: [ScheduleController],
 })
 export class ScheduleModule {}
